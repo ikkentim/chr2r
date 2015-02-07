0a093d6923cad314c8e9f510bea9b7f83f656dd6
Luuk Holleman <luukholleman@gmail.com>
2015-02-07 14:45:53 +0100

    Always apply gravity

--- Heros/Actor.cpp
+++ Heros/Actor.cpp
@@ -6,16 +6,7 @@ Actor::Actor() :velocity_({}), GameObject() {}
 Actor::Actor(Vector2 pos, Vector2 size) : velocity_({}), GameObject(pos, size) {}
 
 void Actor::Falling(Vector2 grav, float delta){
-
-	//if (!onGround) {
-		velocity_ += (grav * delta);
-	//}
-	/*else if (onGround) {
-		if (velocity_.y > 2)
-			position_.y -= velocity_.y;
-
-		velocity_.y -= velocity_.y;
-	}*/
+	velocity_ += (grav * delta);
 }
 
 
