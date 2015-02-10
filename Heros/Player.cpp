42fab8dca7601d9a1089580b619fd644b834c5bd
getro <lucasbudai@gmail.com>
2015-02-10 16:13:29 +0100

    AnimationCharacter_flickering

--- Heros/Player.cpp
+++ Heros/Player.cpp
@@ -40,62 +40,84 @@ void Player::Update(double delta, Keys keys) {
 
 	if (velocity_ == Vector2(0, 0)){
 		//Draw animation when you don't move
+		if (state != DONT_MOVE){
+			state = DONT_MOVE;
+			animationIndex = 0;
+		}
         if (animationTime > 1){
             ++animationIndex;
             animationTime = 0;
-            state = DONT_MOVE;
         }
 	}
     else if (velocity_.x > 0 && IsOnGround() == true){
 		//Draw animation when you go right
+		if (state != GO_RIGHT){
+			state = GO_RIGHT;
+			animationIndex = 0;
+		}
         if (animationTime > 1){
             ++animationIndex;
             animationTime = 0;
-            state = GO_RIGHT;
         }
 	}
     else if (velocity_.x < 0 && IsOnGround() == true){
 		//Draw animation when you go left
+		if (state != GO_LEFT){
+			state = GO_LEFT;
+			animationIndex = 0;
+		}
         if (animationTime > 1){
             ++animationIndex;
             animationTime = 0;
-            state = GO_LEFT;
         }
 	}
     else if (IsOnGround() == false && velocity_.y > 0 && velocity_.x >0){
 		//Draw animation when you jump right
+		if (state != JUMP_RIGHT){
+			state = JUMP_RIGHT;
+			animationIndex = 0;
+		}
+
         if (animationTime > 1){
             ++animationIndex;
             animationTime = 0;
-            state = JUMP_RIGHT;
         }
 	}
     else if (IsOnGround() == false && velocity_.y < 0 && velocity_.x >0){
 		//Draw animation when you fall right
+		if (state != FALL_RIGHT){
+			state = FALL_RIGHT;
+			animationIndex = 0;
+		}
         if (animationTime > 1){
             ++animationIndex;
             animationTime = 0;
-            state = FALL_RIGHT;
         }
 	}
     else if (IsOnGround() == false && velocity_.y > 0 && velocity_.x <0){
 		//Draw animation when you jump left
+		if (state != JUMP_LEFT){
+			state = JUMP_LEFT;
+			animationIndex = 0;
+		}
         if (animationTime > 1){
             ++animationIndex;
             animationTime = 0;
-            state = JUMP_LEFT;
         }
 	}
     else if (IsOnGround() == false && velocity_.y < 0 && velocity_.x <0){
 		//Draw animation when you fall left
+		if (state != FALL_LEFT){
+			state = FALL_LEFT;
+			animationIndex = 0;
+		}
 		if (animationTime > 1){
 			++animationIndex;
 			animationTime = 0;
-			state = FALL_LEFT;
 		}
 	}
-	if (animationIndex >= 4){
-		animationIndex = 0;
+		if (animationIndex >= 4){
+			animationIndex = 0;
 	}
 }
 
@@ -109,15 +131,32 @@ void Player::Render(Viewport &vp) {
 		animationIndex * 24, 40,
 		24, 40
     };
+	//TODO: Create Jump right and Jump left Texture
+	Texture texture_Jump = {
+		animationIndex * 24, 120,
+		24, 40
+	};
+	Texture texture_DontMove = {
+		animationIndex * 24, 0,
+		24, 40
+	};
+	
+
 
 	if (state == GO_RIGHT)
-    SpriteSheet::character->Draw(texture_Right, position_, vp);
+		SpriteSheet::character->Draw(texture_Right, position_, vp);
 
 	if (state == GO_LEFT)
 		SpriteSheet::character->Draw(texture_Left, position_, vp);
+	if (state == JUMP_LEFT){
+		SpriteSheet::character->Draw(texture_Jump, position_, vp);
+	}
+	if (state == DONT_MOVE){
+		SpriteSheet::character->Draw(texture_DontMove, position_, vp);
+	}
 
 
     /* Temporary texture! Remove when animated character works completely!
      * (or when you are working on the animated character) */
-    SpriteSheet::character->Draw(Texture(0 * 24, 40, 24, 40), position_, vp);
+   // SpriteSheet::character->Draw(Texture(0 * 24, 40, 24, 40), position_, vp);
 }
