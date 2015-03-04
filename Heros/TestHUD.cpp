5ecb731bc3c4c2a6cf2615e907b85a84ca2e4275
Luuk Holleman <luukholleman@gmail.com>
2015-03-04 12:10:27 +0100

    ITS PARTEH TIME

--- Heros/TestHUD.cpp
+++ Heros/TestHUD.cpp
@@ -4,5 +4,5 @@ void TestHUD::Update(GameScene *scene , double delta, Keys keys) {
 
 }
 void TestHUD::Render(HDC gr) {
-    TextOut(gr, 5, 40, "Test", 4);
+    TextOut(gr, 5, 40, "This is the HUD HUD HUD", 4);
 }
