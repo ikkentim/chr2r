9282deb0f09f9fedda19b3967754f333a39672a4
Luuk Holleman <luukholleman@gmail.com>
2015-03-04 12:40:50 +0100

    ITS A REAL PARTEH NOW

--- Heros/TestHUD.cpp
+++ Heros/TestHUD.cpp
@@ -4,5 +4,5 @@ void TestHUD::Update(GameScene *scene , double delta, Keys keys) {
 
 }
 void TestHUD::Render(HDC gr) {
-    TextOut(gr, 5, 40, "This is the HUD HUD HUD", 4);
+    TextOut(gr, 200, 10, "This is the HUD HUD HUD", 23);
 }
